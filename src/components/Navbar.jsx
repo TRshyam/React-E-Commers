87b62b9a83ef7eac1d2c2dcd19ce9bbc0f15f5e5
NakulanT <nakult721@gmail.com>
2024-03-27 21:29:07 +0530

    Added html for Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className='NavBar'>
+      <div className='Logo'>ebart</div>
+      <div className='Functions'>
+        <a>Departments</a>
+        <a>Services</a>
+      </div>
+      <div className='ProductSearch'>
+        <form>
+          <input placeholder='Search'></input>
+          <button type='summit'></button>
+        </form>
+      </div>
+      <div className='NavIcons'>
+        <a>Cart</a>
+        <a>Profile</a>
+      </div>
     </div>
   )
 }
