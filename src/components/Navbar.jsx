ded741466baaff0b70ba0cb48d2def360f0a3b8b
NakulanT <nakult721@gmail.com>
2024-03-28 12:34:16 +0530

    Added positions to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
-import './Navbar.css'
+import './Navbar.css';
+import { CiSearch } from "react-icons/ci";
+import { CiShoppingCart } from "react-icons/ci";
+import { CgProfile } from "react-icons/cg";
 
 export default function Navbar() {
   return (
-<<<<<<< HEAD
     <div className='NavBar'>
       <div className='Logo'>ebart</div>
       <div className='Functions'>
         <a>Departments</a>
-        <a>Services</a>
+        <a>Services </a>
       </div>
-      <div className='ProductSearch'>
-        <form>
-          <input placeholder='Search'></input>
-          <button type='summit'></button>
-        </form>
-      </div>
+        
       <div className='NavIcons'>
-        <a>Cart</a>
-        <a>Profile</a>
+        <CiSearch  className='CiSearch'/>
+        <input placeholder='Search..'></input>
+        <button>Login</button>
+        <a><CiShoppingCart /></a>
+        <a><CgProfile /></a>
       </div>
-=======
-    <div>
-      <h1 className='bg-regal-blue-ad'>Navbar</h1>
->>>>>>> f5a1420d753f39e2711786c6ccae232fee2d3e69
-    </div>
-  )
+      </div>
+  );
 }
