36a4f0150b0bb9cb82b992fc86d63031a09e4c42
Yogesh M V <134631738+yogeshwenwu@users.noreply.github.com>
2024-03-29 11:48:57 +0530

    Update Card.jsx

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -8,15 +8,15 @@ export default function Card({ imageSrc, content }) {
     return(
         <>
 <div className='ItemcardWH bg-card-color rounded-md relative overflow-hidden shadow-xl m-6 hover:scale-105 hover:shadow-2xl hover:bgb transition-transform duration-300'>
-    <div className='space-y-4  relative'>
+    <div className='w-36 h-48 relative '>
         <div className='relative '>
             <img src={imageSrc} alt="" className='' />
         </div>
         
-        <div className='absolute top-3/4 w-full' >
+        <div className='absolute pt-2 top-2/3 w-full' >
             <div className=' flex flex-col md:flex-row items-center justify-center md:justify-around '>
-                <div className='text-center'>
-                    <h6>Omen 16</h6>
+                <div className='text-center text-sm'>
+                    <p>Omen 16</p>
                     <h6>8 items</h6>
                 </div>
                 <div className='bg-white p-2 rounded-3xl hover:bg-black hover:text-white'>
