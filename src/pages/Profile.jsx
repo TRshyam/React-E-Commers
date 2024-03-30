f4e8b4d6f6eef3e2580c345a7f4486b5fd84a9f7
Yogesh M V <134631738+yogeshwenwu@users.noreply.github.com>
2024-03-30 09:09:36 +0530

    Create Profile.jsx

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+export default function Profile() {
+  return (
+    <>
+        <Navbar/>
+        <Footer/>
+    </>
+  )
+}
