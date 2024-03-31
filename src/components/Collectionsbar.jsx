484d7ee08d640391d5734bb50e580a9c244e568d
NakulanT <nakult721@gmail.com>
2024-03-31 18:25:16 +0530

    Your commit message here

--- src/components/Collectionsbar.jsx
+++ src/components/Collectionsbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Pot1 from '../assets/CardItems/Pot-1.jpg'
 import Pot2 from '../assets/CardItems/Pot-2.jpg'
 import Pot3 from '../assets/CardItems/Pot-3.jpg'
@@ -9,56 +9,60 @@ import Sofa3 from '../assets/CardItems/Sofa-3.jpg'
 import Sofa4 from '../assets/CardItems/Sofa-4.jpg'
 import './CollectionsBarImageRendering.css'
 import { FaArrowRight } from "react-icons/fa";
-
+import { GrNext, GrLinkPrevious } from "react-icons/gr";
 
 const CollectionsBar = () => {
+    const BannerImages = [Pot1, Pot2, Pot3, Pot4, Sofa1, Sofa2, Sofa3, Sofa4];
     return (
         <>
-            {CollectionsBarImageRendering({
-                FirstImage: Pot4,
-                SecondImage: Pot2,
-                ThirdImage: Pot3,
-                FourthImage: Pot1,
-                Title: "Plant Pot",
-                Offer: "20"
-            })}
-            <br></br>
-
-{CollectionsBarImageRendering({
-                FirstImage: Sofa1,
-                SecondImage: Sofa2,
-                ThirdImage: Sofa3,
-                FourthImage: Sofa4,
-                Title: "Cusion Sofa",
-                Offer: "20"
-            })}
+            <CollectionsBarImageRendering
+                Images={BannerImages}
+                Title="Plant Pot and Sofa"
+                Offer="20"
+            />
+            <br />
         </>
     );
 }
 
 const CollectionsBarImageRendering = (props) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const HandleNext = () => {
+        setCurrentIndex((currentIndex + 1) % props.Images.length);
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % props.Images.length);
+        }, 5000); // Slide every 3 seconds
+
+        return () => clearInterval(interval);
+    }, [props.Images.length]);
+
     return (
         <div className="CollectionsBanner">
             <div className="FirstBanner">
                 <h2>Amazing Collections</h2>
                 <h1>{props.Title}</h1>
-                <button>Shop Now <FaArrowRight className="RightArrow"/></button>
-                <img src={props.FirstImage} alt="IMAGE" />
-                <div className="OfferDetails" >
-                <h3>{props.Offer} % off</h3>
-                <h4>|</h4>
-                <a>See Offers</a>
+                <button>Shop Now <FaArrowRight className="RightArrow" /></button>
+                <img src={props.Images[currentIndex]} alt="IMAGE" />
+                <div className="OfferDetails">
+                    <h3>{props.Offer} % off</h3>
+                    <h4>|</h4>
+                    <a>See Offers</a>
                 </div>
             </div>
             <div className="SubBanner">
-                <img src={props.SecondImage} alt="IMAGE" />
+                <img src={props.Images[(currentIndex + 1) % props.Images.length]} alt="IMAGE" />
             </div>
             <div className="SubBanner">
-                <img src={props.ThirdImage} alt="IMAGE" />
+                <img src={props.Images[(currentIndex + 2) % props.Images.length]} alt="IMAGE" />
             </div>
             <div className="SubBanner">
-                <img src={props.FourthImage} alt="IMAGE" />
+                <img src={props.Images[(currentIndex + 3) % props.Images.length]} alt="IMAGE" />
             </div>
+            <button className="NextButton" onClick={HandleNext}><GrLinkPrevious/></button>
         </div>
     );
 }
