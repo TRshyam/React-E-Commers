f25b6f90d39629488018fecf8d260b23840d768c
SHYAM T.R <t.r.shyam0007@gmail.com>
2024-04-16 01:15:27 +0530

    similar,recently( card arrangement)

--- client/src/components/Cards/Card.jsx
+++ client/src/components/Cards/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default function Card({ imageSrc, content, id }) {
     return (
-        <div className='w-[90%] h-[250px] xl:h-[300px] rounded-md relative overflow-hidden shadow-xl m-6 transform transition duration-300 hover:scale-105 hover:shadow-2xl hover:bg-gray-100'>
+        <div className='w-[90%] h-[250px] xl:h-[300px] rounded-md relative overflow-hidden shadow-xl m-6 transform transition duration-300 hover:scale-105 hover:shadow-2xl hover:bg-gray-100 select-none'>
             {/* <h1 className='text-fuchsia-700'>{`127.0.0.1:5000${imageSrc}`}</h1> */}
             <div className='h-[200px] xl:h-[250px]'>
                 {/* Image */}
--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -3,22 +3,35 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import ProductImages from '../components/ProductPage/ProductImage';
 import ProductDetails from '../components/ProductPage/ProductDetails';
-import Button from '../components/CartButton';
 import Navbar from '../components/Navbar'
+import Carousel from '../components/carousel/Carousel';
+import Card from '../components/Cards/Card';
+
 
 
 function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+  const [ReleatedProducts,setReleatedProducts]=useState()
   const [loading, setLoading] = useState(true);
 
+  const content =
+  {content: {
+         product: "Pixel ",
+         prize: "50",
+       }
+      }
+      console.log(content.content);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('http://localhost:5000/api/data');
         const foundProduct = response.data[id];
+        const products= response.data
+
         setProduct(foundProduct);
         setLoading(false);
+        setReleatedProducts(products)
       } catch (error) {
         console.error(`Error fetching product: ${error}`);
         setLoading(false);
@@ -29,21 +42,63 @@ function ProductPage() {
 
   }, [id]);
 
+  const itemCards = {};
+for (const key in ReleatedProducts) {
+  if (ReleatedProducts.hasOwnProperty(key)) {
+    const card = ReleatedProducts[key];
+    if (card.cardType === 'item') {
+      itemCards[key] = card;
+    }
+  }
+}
+console.log(itemCards);
+
+// Render ItemCards
+const renderItemCards = (itemCards) => {
+  return Object.keys(itemCards).map((key) => {
+    const { img, content, id } = itemCards[key];
+    return <Card key={key} imageSrc={img} content={content} id={id} />;
+  });
+};
+
+
   if (loading) return <p>Loading...</p>;
   if (!product) return <p>Product not found</p>;
 
   return (
     <div>
       <Navbar/>
-      <div className='w-screen h-screen bg-violet-500'>
+      <div className='w-full h-screen bg-violet-500'>
         <div className='mx-0 xl:mx-16 2xl:mx-48 py-3 h-auto bg-fuchsia-300 flex'>
             
             <ProductImages mainImgs={product.main_imgs} />
 
             <ProductDetails description={product.description} />
         </div>
-        <div>
-          
+        <div className='bg-orange-300 h-full mx-0 xl:mx-16 2xl:mx-48 py-3'>
+          <div className='bg-pink-400  '>
+            <span className='text-xl font-semibold'>Similar Products</span>
+            <Carousel>
+                {renderItemCards(itemCards)}
+            </Carousel>         
+          </div>
+          <div className='bg-pink-400  '>
+            <Carousel>
+                {renderItemCards(itemCards)}
+            </Carousel>         
+          </div>
+          <div className='bg-pink-400 my-5  '>
+            <span className='text-xl font-semibold '>Recently Viewed</span>
+            <Carousel>
+                {renderItemCards(itemCards)}
+            </Carousel>         
+          </div>
+          {/* <div className='bg-pink-800 grid grid-cols-3 gap-4 md:hidden '> 
+            {renderItemCards(itemCards)}
+          </div> */}
+          <div>
+            
+          </div>
         </div>
       </div>
     </div>
