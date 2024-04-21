8da6bc627a1cf9f9978ec4cc391fd92b0a846428
gowthamrajlegend <gowthamrajlegend@gmail.com>
2024-04-21 07:12:36 +0530

    no changes

--- client/src/components/CartButton.jsx
+++ client/src/components/CartButton.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '@mui/material/Button';
 import './CartButton.css';
 
 function CartButton() {
--- client/src/components/ProductPage/ProductImage.jsx
+++ client/src/components/ProductPage/ProductImage.jsx
@@ -1,4 +1,3 @@
-import { Button } from '@mui/material';
 import React from 'react';
 import CartButton from '../CartButton';
 
